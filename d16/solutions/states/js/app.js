fee35afc9b45f963e8ab3d15af1c997caa650bc3
Jeffrey Konowitch <jkonowitch@gmail.com>
2016-12-08 20:44:00 -0500

    states

--- d16/solutions/states/js/app.js
+++ d16/solutions/states/js/app.js
@@ -16,10 +16,14 @@ $("button#ct").on("click", function() {
   $("h1").text(ct["name"]);
   $("span.capital").text(ct["capital"]);
   $("span.governor").text(ct["governor"]);
+  $("li#first-senator").text(ct["usSenators"][0]);
+  $("li#second-senator").text(ct["usSenators"][1]);
 });
 
 $("button#ny").on("click", function() {
   $("h1").text(ny["name"]);
   $("span.capital").text(ny["capital"]);
   $("span.governor").text(ny["governor"]);
+    $("li#first-senator").text(ny["usSenators"][0]);
+  $("li#second-senator").text(ny["usSenators"][1]);
 });
